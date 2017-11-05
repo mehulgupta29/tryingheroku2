6da447e209e07a95e88b422ecb3c617601f5ccb8
Mehul Gupta <MehulGupta@Mehuls-MBP.fios-router.home>
2017-11-05 12:30:02 -0500

    First commit - project setup

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Trying react-webpack-redux and deploying it on Heruko</h1>
+      </div>
+    )
+  }
+}
